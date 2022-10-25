c1605c5ba2935db62f4b6d170b5c5c4f3fa5fa81
Cerdtruir <ghoornabeel@gmail.com>
2022-10-25 19:52:22 +0200

    Change db to psql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_22_094304) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flights", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
