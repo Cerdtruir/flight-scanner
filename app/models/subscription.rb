5fa56e5de10e5733f3fae32b24ab603db32e6a9d
Cerdtruir <ghoornabeel@gmail.com>
2022-10-27 15:56:01 +0200

    Add subscription validations in fom and model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,7 @@
 class Subscription < ApplicationRecord
+  validates :email, presence: true
+  validates :origin, presence: true
+  validates :destination, presence: true
+  validates :date_start, presence: true
+  validates :date_end, presence: true
 end
